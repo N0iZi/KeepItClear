a460fe4232972889b3d33cea26479b832930ae76
Andriy Shelest <MrAShelest@gmail.com>
2019-02-13 00:48:10 +0100

    Code of "cleanUp" method in "Cleaner" class moved to "Remover" class. Method "cleanUp" in "Cleaner" class declared as abstract.

--- src/main/java/ashelest/project/keepitclear/Cleaner.java
+++ src/main/java/ashelest/project/keepitclear/Cleaner.java
@@ -1,12 +1,8 @@
 package ashelest.project.keepitclear;
 
-import javax.swing.*;
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileWriter;
-import java.io.IOException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
 
 public abstract class Cleaner {
@@ -24,44 +20,7 @@ public abstract class Cleaner {
         desktop = new Desktop();
     }
 
-    public boolean cleanUp(long expired, boolean logEnabled) {
-        ArrayList<String> files;
-        boolean result = true;
-        boolean toLog = logEnabled;
-        try {
-            files = desktop.getFilesToDelete(expired);
-        } catch (IOException e) {
-            e.printStackTrace();
-            return false;
-        }
-        for (String file :
-                files) {
-            File toDelete = new File(file);
-            if (toDelete.delete()) {
-                if (toLog) {
-                    try {
-                        logAction(file, LogRows.DELETED);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                        JOptionPane.showMessageDialog(null, "Unfortunately, something went wrong while logging!\nLogging if off now. Please check the folder with application is without \"Read only\" flag checked.", "KeepItClear: Logging error!", JOptionPane.ERROR_MESSAGE);
-                        toLog = false;
-                    }
-                }
-            } else {
-                if (toLog) {
-                    try {
-                        logAction(file, LogRows.FAIL);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                        JOptionPane.showMessageDialog(null, "Unfortunately, something went wrong while logging!\nLogging if off now. Please check the folder with application is without \"Read only\" flag checked.", "KeepItClear: Logging error!", JOptionPane.ERROR_MESSAGE);
-                        toLog = false;
-                    }
-                }
-                result = false;
-            }
-        }
-        return result;
-    }
+    public abstract boolean cleanUp(long expired, boolean logEnabled);
 
     public void logAction(String filepath, LogRows result) throws Exception {
         BufferedWriter bufferedWriter;
--- src/main/java/ashelest/project/keepitclear/Remover.java
+++ src/main/java/ashelest/project/keepitclear/Remover.java
@@ -1,5 +1,10 @@
 package ashelest.project.keepitclear;
 
+import javax.swing.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Remover extends Cleaner {
 
     public Remover() {
@@ -8,7 +13,42 @@ public class Remover extends Cleaner {
 
     @Override
     public boolean cleanUp(long expired, boolean logEnabled) {
-        return super.cleanUp(expired, logEnabled);
+        ArrayList<String> files;
+        boolean result = true;
+        boolean toLog = logEnabled;
+        try {
+            files = desktop.getFilesToDelete(expired);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+        for (String file :
+                files) {
+            File toDelete = new File(file);
+            if (toDelete.delete()) {
+                if (toLog) {
+                    try {
+                        logAction(file, LogRows.DELETED);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        JOptionPane.showMessageDialog(null, "Unfortunately, something went wrong while logging!\nLogging if off now. Please check the folder with application is without \"Read only\" flag checked.", "KeepItClear: Logging error!", JOptionPane.ERROR_MESSAGE);
+                        toLog = false;
+                    }
+                }
+            } else {
+                if (toLog) {
+                    try {
+                        logAction(file, LogRows.FAIL);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        JOptionPane.showMessageDialog(null, "Unfortunately, something went wrong while logging!\nLogging if off now. Please check the folder with application is without \"Read only\" flag checked.", "KeepItClear: Logging error!", JOptionPane.ERROR_MESSAGE);
+                        toLog = false;
+                    }
+                }
+                result = false;
+            }
+        }
+        return result;
     }
 
 }
